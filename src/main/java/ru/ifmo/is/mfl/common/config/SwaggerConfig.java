8ebb32d2f3c6e6a5c91231513c875d3a06fe882d
maxbarsukov <maximbarsukov@bk.ru>
2025-01-04 00:53:33 +0300

    feat: :sparkles: add OpenAPI config

---
+++ src/main/java/ru/ifmo/is/mfl/common/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package ru.ifmo.is.mfl.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+  title = "MyFilList | Information Systems",
+  description = "Spring Boot API for MyFilmList",
+  version = "0.1.0")
+)
+@SecurityScheme(
+  name = "bearerAuth",
+  type = SecuritySchemeType.HTTP,
+  bearerFormat = "JWT",
+  scheme = "bearer"
+)
+public class SwaggerConfig {
+}
