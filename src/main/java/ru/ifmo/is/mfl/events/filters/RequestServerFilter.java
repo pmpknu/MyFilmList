b793f281395466c22890200e43c6a51e20d0b8d1
maxbarsukov <maximbarsukov@bk.ru>
2025-01-04 23:41:57 +0300

    feat: :sparkles: set `X-Response-Uuid` header

---
+++ src/main/java/ru/ifmo/is/mfl/events/filters/RequestServerFilter.java
@@ -0,0 +1,29 @@
+package ru.ifmo.is.mfl.events.filters;
+
+import lombok.NonNull;
+import com.fasterxml.uuid.Generators;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class RequestServerFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(
+    @NonNull HttpServletRequest request,
+    @NonNull HttpServletResponse response,
+    @NonNull FilterChain filterChain
+  ) throws ServletException, IOException {
+    var uuid = Generators.timeBasedEpochRandomGenerator().generate().toString();
+
+    request.setAttribute("requestUUID", uuid);
+    response.setHeader("X-Response-Uuid", uuid);
+
+    filterChain.doFilter(request, response);
+  }
+}
