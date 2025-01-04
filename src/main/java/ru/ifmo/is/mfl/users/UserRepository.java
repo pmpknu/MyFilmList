a5e0f70c8c500e33c7b5711555c72b243368c5ba
maxbarsukov <maximbarsukov@bk.ru>
2025-01-04 03:28:23 +0300

    refactor: :recycle: remove unused user repository method

--- src/main/java/ru/ifmo/is/mfl/users/UserRepository.java
+++ src/main/java/ru/ifmo/is/mfl/users/UserRepository.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User> {
   Optional<User> findByUsername(String username);
   Optional<User> findByEmail(String email);
-  Optional<User> findByPassword(String password);
   boolean existsByUsername(String username);
   boolean existsByEmail(String email);
 }
