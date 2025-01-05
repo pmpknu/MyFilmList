955d5302bec1b37089a407a9bae8a981197da9d2
maxbarsukov <maximbarsukov@bk.ru>
2025-01-06 02:35:47 +0300

    feat: :sparkles: add password reset service

---
+++ src/main/java/ru/ifmo/is/mfl/passwordreset/PasswordResetTokenService.java
@@ -0,0 +1,53 @@
+package ru.ifmo.is.mfl.passwordreset;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.ifmo.is.mfl.common.errors.TokenExpiredException;
+import ru.ifmo.is.mfl.common.utils.crypto.TokenGenerator;
+import ru.ifmo.is.mfl.users.User;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetTokenService {
+
+  @Value("${app.auth.password_reset_token.ttl}")
+  private String passwordResetTokenTtl;
+
+  @Value("${app.auth.password_reset_token.length}")
+  private String passwordResetTokenLength;
+
+  private final PasswordResetTokenRepository repository;
+  private final TokenGenerator tokenGenerator;
+
+  public Optional<PasswordResetToken> findByToken(String token) {
+    return repository.findByToken(token);
+  }
+
+  public Optional<PasswordResetToken> findByUser(User user) {
+    return repository.findByUser(user);
+  }
+
+  public PasswordResetToken createPasswordResetToken(User user) {
+    var token = tokenGenerator.generateSecureToken(Integer.parseInt(passwordResetTokenLength));
+    var passwordResetToken = PasswordResetToken.builder()
+      .user(user)
+      .date(Instant.now())
+      .token(token)
+      .build();
+
+    return repository.save(passwordResetToken);
+  }
+
+  public PasswordResetToken verifyExpiration(PasswordResetToken token) {
+    if (token.getDate().plusMillis(Long.parseLong(passwordResetTokenTtl)).compareTo(Instant.now()) < 0) {
+      repository.delete(token);
+      throw new TokenExpiredException("Password reset token was expired. Please make a new password reset request");
+    }
+
+    return token;
+  }
+}
