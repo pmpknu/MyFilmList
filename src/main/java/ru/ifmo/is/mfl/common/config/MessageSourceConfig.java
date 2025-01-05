d4331dba275255d4a5d43fdb50aa60cd9667ece4
maxbarsukov <maximbarsukov@bk.ru>
2025-01-05 03:42:01 +0300

    chore: :wrench: configurate `MessageSource`

---
+++ src/main/java/ru/ifmo/is/mfl/common/config/MessageSourceConfig.java
@@ -0,0 +1,17 @@
+package ru.ifmo.is.mfl.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+  @Bean
+  public ReloadableResourceBundleMessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:messages");
+    messageSource.setCacheSeconds(3600);
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+}
