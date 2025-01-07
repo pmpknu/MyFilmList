5296091abe5100cf60964d2eb77bbcd6f54bd921
pmpknu <dangorlyakov@gmail.com>
2025-01-07 08:42:37 +0300

    feat(back): add rating entity

---
+++ src/main/java/ru/ifmo/is/mfl/ratings/Rating.java
@@ -0,0 +1,43 @@
+package ru.ifmo.is.mfl.ratings;
+
+import ru.ifmo.is.mfl.common.framework.CrudEntity;
+import ru.ifmo.is.mfl.movies.Movie;
+import ru.ifmo.is.mfl.users.User;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ratings")
+public class Rating extends CrudEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ratings_id_seq")
+  @SequenceGenerator(name = "ratings_id_seq", sequenceName = "ratings_id_seq", allocationSize = 1)
+  @Column(name="id", nullable=false, unique=true)
+  private int id;
+
+  @NotNull
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @NotNull
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "movie_id", nullable = false)
+  private Movie movie;
+
+  @Column(name = "value")
+  @Min(1)
+  @Max(10)
+  private int value;
+}
