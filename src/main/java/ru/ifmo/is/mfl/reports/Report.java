1e7c37e3d40bd3c022a489f82cbd90d44b0c7dff
pmpknu <dangorlyakov@gmail.com>
2025-01-07 08:13:35 +0300

    fix(back): change fetch type from lazy to eager in report entity

--- src/main/java/ru/ifmo/is/mfl/reports/Report.java
+++ src/main/java/ru/ifmo/is/mfl/reports/Report.java
@@ -25,7 +25,7 @@ public class Report extends CrudEntity {
   private int id;
 
   @JsonIgnore
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
