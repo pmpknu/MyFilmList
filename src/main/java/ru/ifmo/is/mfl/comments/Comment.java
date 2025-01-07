f0e242b3a7fdf646b6611a5339022cd069ebdac1
pmpknu <dangorlyakov@gmail.com>
2025-01-07 18:28:53 +0300

    fix(back): fold imports in comment entity

--- src/main/java/ru/ifmo/is/mfl/comments/Comment.java
+++ src/main/java/ru/ifmo/is/mfl/comments/Comment.java
@@ -4,16 +4,7 @@ import java.sql.Timestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import ru.ifmo.is.mfl.common.framework.CrudEntity;
 import ru.ifmo.is.mfl.movies.Movie;
