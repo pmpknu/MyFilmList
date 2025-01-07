2e8f4997a82af984f3df14dd387687731409548d
pmpknu <dangorlyakov@gmail.com>
2025-01-07 06:52:18 +0300

    feat(back): add comment entity

---
+++ src/main/java/ru/ifmo/is/mfl/comments/Comment.java
@@ -0,0 +1,71 @@
+package ru.ifmo.is.mfl.comments;
+
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.*;
+import ru.ifmo.is.mfl.common.framework.CrudEntity;
+import ru.ifmo.is.mfl.movies.Movie;
+import ru.ifmo.is.mfl.reports.Report;
+import ru.ifmo.is.mfl.users.User;
+import ru.ifmo.is.mfl.watchlists.WatchList;
+import ru.ifmo.is.mfl.reviews.Review;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comment extends CrudEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comments_id_seq")
+    @SequenceGenerator(name = "comments_id_seq", sequenceName = "comments_id_seq", allocationSize = 1)
+    @Column(name="id", nullable=false, unique=true)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "review_id", referencedColumnName = "id", nullable = false)
+    private Review review;
+
+    @Column(name = "visible", nullable = false)
+    private Boolean visible;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "date", nullable = false)
+    private Timestamp date;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report report;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "watchlist_id", nullable = false)
+    private WatchList watchList;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+}
