c19a01be6fe3c80f7c8030c8cff3eca2979d1ee2
pmpknu <dangorlyakov@gmail.com>
2025-01-07 07:22:05 +0300

    feat(back): add review entity

---
+++ src/main/java/ru/ifmo/is/mfl/reviews/Review.java
@@ -0,0 +1,60 @@
+package ru.ifmo.is.mfl.reviews;
+
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.BatchSize;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ru.ifmo.is.mfl.common.framework.CrudEntity;
+import ru.ifmo.is.mfl.movies.Movie;
+import ru.ifmo.is.mfl.users.User;
+import ru.ifmo.is.mfl.ratings.Rating;
+import ru.ifmo.is.mfl.comments.Comment;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "reviews")
+public class Review extends CrudEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reviews_id_seq")
+  @SequenceGenerator(name = "reviews_id_seq", sequenceName = "reviews_id_seq", allocationSize = 1)
+  @Column(name="id", nullable=false, unique=true)
+  private int id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "movie_id", referencedColumnName = "id", nullable = false)
+  private Movie movie;
+
+  @Column(name = "visible", nullable = false)
+  private Boolean visible;
+
+  @Column(name = "text", nullable = false)
+  private String text;
+
+  @Column(name = "date", nullable = false)
+  private Timestamp date;
+
+  @JsonManagedReference
+  @BatchSize(size = 50)
+  @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "review", orphanRemoval = true)
+  private Set<Rating> ratings = new HashSet<>();
+
+  @JsonManagedReference
+  @BatchSize(size = 50)
+  @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "review", orphanRemoval = true)
+  private Set<Comment> comments = new HashSet<>();
+}
