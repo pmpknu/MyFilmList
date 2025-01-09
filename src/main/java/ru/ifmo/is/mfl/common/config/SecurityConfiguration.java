ed9362f1d35151fd439f3315c256fb05745a95ef
maxbarsukov <maximbarsukov@bk.ru>
2025-01-09 18:41:52 +0300

    chore: :wrench: mark unimplemented endpoints as TODOs

--- src/main/java/ru/ifmo/is/mfl/common/config/SecurityConfiguration.java
+++ src/main/java/ru/ifmo/is/mfl/common/config/SecurityConfiguration.java
@@ -71,57 +71,57 @@ public class SecurityConfiguration {
           .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
 
           // Feed
-          .requestMatchers(HttpMethod.GET, "/api/feed/**").authenticated() // Every registered user can view news feed
+          .requestMatchers(HttpMethod.GET, "/api/feed/**").authenticated() // Every registered user can view news feed // TODO
 
           // Reports
-          .requestMatchers(HttpMethod.POST, "/api/reviews/*/reports").hasRole("USER") // User can report review
-          .requestMatchers(HttpMethod.POST, "/api/comments/*/reports").hasRole("USER") // User can report comment
-          .requestMatchers(HttpMethod.GET, "/api/reports/**").hasAnyRole("ADMIN", "MODERATOR") // Admin can view all report
-          .requestMatchers(HttpMethod.GET, "/api/reports/pending").hasAnyRole("ADMIN", "MODERATOR") // Admin can view pending report
-          .requestMatchers(HttpMethod.PATCH, "/api/reports/**").hasAnyRole("ADMIN", "MODERATOR") // Admin can update reports
+          .requestMatchers(HttpMethod.POST, "/api/reviews/*/reports").hasRole("USER") // User can report review // TODO
+          .requestMatchers(HttpMethod.POST, "/api/comments/*/reports").hasRole("USER") // User can report comment // TODO
+          .requestMatchers(HttpMethod.GET, "/api/reports/**").hasAnyRole("ADMIN", "MODERATOR") // Admin can view all report // TODO
+          .requestMatchers(HttpMethod.GET, "/api/reports/pending").hasAnyRole("ADMIN", "MODERATOR") // Admin can view pending report // TODO
+          .requestMatchers(HttpMethod.PATCH, "/api/reports/**").hasAnyRole("ADMIN", "MODERATOR") // Admin can update reports // TODO
 
           // Movie views
-          .requestMatchers(HttpMethod.GET, "/api/users/*/views").permitAll() // Get all user's viewed movies
-          .requestMatchers(HttpMethod.GET, "/api/movies/*/views").permitAll() // Get all movie's viewers
-          .requestMatchers(HttpMethod.POST, "/api/movies/*/views").hasRole("USER") // Mark movie as viewed
-          .requestMatchers(HttpMethod.DELETE, "/api/movies/*/views").hasRole("USER") // Unmark viewed movie
+          .requestMatchers(HttpMethod.GET, "/api/users/*/views").permitAll() // Get all user's viewed movies // TODO
+          .requestMatchers(HttpMethod.GET, "/api/movies/*/views").permitAll() // Get all movie's viewers // TODO
+          .requestMatchers(HttpMethod.POST, "/api/movies/*/views").hasRole("USER") // Current user can mark movie as viewed // TODO
+          .requestMatchers(HttpMethod.DELETE, "/api/movies/*/views").hasRole("USER") // Current user can unmark viewed movie // TODO
 
           // Comments
-          .requestMatchers(HttpMethod.GET, "/api/movies/*/comments").permitAll() // Get all movie comments
-          .requestMatchers(HttpMethod.GET, "/api/reviews/*/comments").permitAll() // Get all review comments
-          .requestMatchers(HttpMethod.GET, "/api/watchlists/*/comments").permitAll() // Get all watchlist comments
-          .requestMatchers(HttpMethod.POST, "/api/movies/*/comments").hasRole("USER") // Add comment to movie
-          .requestMatchers(HttpMethod.POST, "/api/reviews/*/comments").hasRole("USER") // Add comment to review
-          .requestMatchers(HttpMethod.POST, "/api/watchlists/*/comments").hasRole("USER") // Add comment to watchlist
-          .requestMatchers(HttpMethod.PATCH, "/api/comments/**").hasRole("USER") // Change comment by ID
-          .requestMatchers(HttpMethod.DELETE, "/api/comments/**").hasRole("USER") // Delete comment by ID
+          .requestMatchers(HttpMethod.GET, "/api/movies/*/comments").permitAll() // Get all movie comments // TODO
+          .requestMatchers(HttpMethod.GET, "/api/reviews/*/comments").permitAll() // Get all review comments // TODO
+          .requestMatchers(HttpMethod.GET, "/api/watchlists/*/comments").permitAll() // Get all watchlist comments // TODO
+          .requestMatchers(HttpMethod.POST, "/api/movies/*/comments").hasRole("USER") // Add comment to movie // TODO
+          .requestMatchers(HttpMethod.POST, "/api/reviews/*/comments").hasRole("USER") // Add comment to review // TODO
+          .requestMatchers(HttpMethod.POST, "/api/watchlists/*/comments").hasRole("USER") // Add comment to watchlist // TODO
+          .requestMatchers(HttpMethod.PATCH, "/api/comments/**").hasRole("USER") // Change comment by ID // TODO
+          .requestMatchers(HttpMethod.DELETE, "/api/comments/**").hasRole("USER") // Delete comment by ID // TODO
 
           // Ratings
-          .requestMatchers(HttpMethod.GET, "/api/movies/*/ratings").hasRole("USER") // Current user's rating
-          .requestMatchers(HttpMethod.POST, "/api/movies/*/ratings").hasRole("USER") // Add movie's rating by current user
-          .requestMatchers(HttpMethod.PATCH, "/api/movies/*/ratings").hasRole("USER") // Change movie's rating by current user
-          .requestMatchers(HttpMethod.DELETE, "/api/movies/*/ratings").hasRole("USER") // Delete movie's rating by current user
+          .requestMatchers(HttpMethod.GET, "/api/movies/*/ratings").hasRole("USER") // Current user's rating // TODO
+          .requestMatchers(HttpMethod.POST, "/api/movies/*/ratings").hasRole("USER") // Add movie's rating by current user // TODO
+          .requestMatchers(HttpMethod.PATCH, "/api/movies/*/ratings").hasRole("USER") // Change movie's rating by current user // TODO
+          .requestMatchers(HttpMethod.DELETE, "/api/movies/*/ratings").hasRole("USER") // Delete movie's rating by current user // TODO
 
           // Reviews
-          .requestMatchers(HttpMethod.GET, "/api/reviews/**").permitAll() // Get all reviews
-          .requestMatchers(HttpMethod.POST, "/api/reviews/search").permitAll() // Search review
-          .requestMatchers(HttpMethod.PATCH, "/api/reviews/**").hasRole("USER") // Change review by ID
-          .requestMatchers(HttpMethod.DELETE, "/api/reviews/**").hasRole("USER") // Delete review by ID
-          .requestMatchers(HttpMethod.GET, "/api/movies/*/reviews").permitAll() // Get all movie's reviews
-          .requestMatchers(HttpMethod.GET, "/api/users/*/reviews").permitAll() // Get all user's reviews
-          .requestMatchers(HttpMethod.POST, "/api/movies/*/reviews").hasRole("USER") // Add review to movie
+          .requestMatchers(HttpMethod.GET, "/api/reviews/**").permitAll() // Get all reviews // TODO
+          .requestMatchers(HttpMethod.POST, "/api/reviews/search").permitAll() // Search review // TODO
+          .requestMatchers(HttpMethod.PATCH, "/api/reviews/**").hasRole("USER") // Change review by ID // TODO
+          .requestMatchers(HttpMethod.DELETE, "/api/reviews/**").hasRole("USER") // Delete review by ID // TODO
+          .requestMatchers(HttpMethod.GET, "/api/movies/*/reviews").permitAll() // Get all movie's reviews // TODO
+          .requestMatchers(HttpMethod.GET, "/api/users/*/reviews").permitAll() // Get all user's reviews // TODO
+          .requestMatchers(HttpMethod.POST, "/api/movies/*/reviews").hasRole("USER") // Add review to movie // TODO
 
           // Movies
-          .requestMatchers(HttpMethod.GET, "/api/movies/**").permitAll()
-          .requestMatchers(HttpMethod.POST, "/api/movies/search").permitAll()
-          .requestMatchers(HttpMethod.POST, "/api/movies/**").hasRole("ADMIN")
-          .requestMatchers(HttpMethod.PATCH, "/api/movies/**").hasRole("ADMIN")
-          .requestMatchers(HttpMethod.POST, "/api/movies/*/poster").hasRole("ADMIN")
-          .requestMatchers(HttpMethod.DELETE, "/api/movies/**").hasRole("ADMIN")
+          .requestMatchers(HttpMethod.GET, "/api/movies/**").permitAll() // TODO
+          .requestMatchers(HttpMethod.POST, "/api/movies/*/poster").hasRole("ADMIN") // TODO
+          .requestMatchers(HttpMethod.POST, "/api/movies/search").permitAll() // TODO
+          .requestMatchers(HttpMethod.POST, "/api/movies/**").hasRole("ADMIN") // TODO
+          .requestMatchers(HttpMethod.PATCH, "/api/movies/**").hasRole("ADMIN") // TODO
+          .requestMatchers(HttpMethod.DELETE, "/api/movies/**").hasRole("ADMIN") // TODO
 
           // Watchlists
-          .requestMatchers(HttpMethod.POST, "/api/watchlists/*/movies/**").hasRole("USER") // Add movie to watchlist
-          .requestMatchers(HttpMethod.DELETE, "/api/watchlists/*/movies/**").hasRole("USER") // Delete movie from watchlist
+          .requestMatchers(HttpMethod.POST, "/api/watchlists/*/movies/**").hasRole("USER") // Add movie to watchlist // TODO
+          .requestMatchers(HttpMethod.DELETE, "/api/watchlists/*/movies/**").hasRole("USER") // Delete movie from watchlist // TODO
 
           // Users
           .requestMatchers(HttpMethod.POST, "/api/users/photo").authenticated()
