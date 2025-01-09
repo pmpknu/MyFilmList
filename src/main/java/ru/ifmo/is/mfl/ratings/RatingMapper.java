bd3ad12504cf94d76314d58a4b153e5d39d81999
maxbarsukov <maximbarsukov@bk.ru>
2025-01-09 22:30:01 +0300

    feat(ratings): :sparkles: add rating mapper

---
+++ src/main/java/ru/ifmo/is/mfl/ratings/RatingMapper.java
@@ -0,0 +1,26 @@
+package ru.ifmo.is.mfl.ratings;
+
+import org.mapstruct.*;
+
+import ru.ifmo.is.mfl.common.framework.CrudMapper;
+import ru.ifmo.is.mfl.common.mapper.JsonNullableMapper;
+import ru.ifmo.is.mfl.common.mapper.ReferenceMapper;
+import ru.ifmo.is.mfl.movies.MovieMapper;
+import ru.ifmo.is.mfl.ratings.dto.*;
+import ru.ifmo.is.mfl.users.UserMapper;
+
+@Mapper(
+  uses = { JsonNullableMapper.class, ReferenceMapper.class, UserMapper.class, MovieMapper.class },
+  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+  componentModel = MappingConstants.ComponentModel.SPRING,
+  unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class RatingMapper implements CrudMapper<Rating, RatingDto, RatingCreateDto, RatingUpdateDto> {
+  public abstract Rating map(RatingCreateDto dto);
+
+  public abstract RatingDto map(Rating model);
+
+  public abstract Rating map(RatingDto model);
+
+  public abstract void update(RatingUpdateDto dto, @MappingTarget Rating model);
+}
