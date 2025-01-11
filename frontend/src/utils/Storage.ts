7731643fbe0101c494a148d2186bc9b5da4eb58e
pmpknu <dangorlyakov@gmail.com>
2025-01-11 16:49:06 +0300

    feat(utils): add utility for managing localStorage

---
+++ frontend/src/utils/Storage.ts
@@ -0,0 +1,60 @@
+/**
+ * Wrapper for convenient work with localStorage.
+ * It can store any data type, not just strings
+ */
+export default class Storage {
+  /**
+   * @static
+   * Check if localStorage is available cause of SSR
+   * @returns {boolean} Is localStorage available
+   * @description Checks if localStorage is available
+   */
+  static isLocalStorageAvailable(): boolean {
+    return typeof window !== 'undefined' && typeof localStorage !== 'undefined';
+  }
+
+  /**
+   * @static
+   * Wrapper for localStorage.clear()
+   * @param {Storage} storage localStorage or sessionStorage
+   * @description Clears the entire localStorage
+   */
+  static clear(storage = localStorage) {
+    storage.clear();
+  }
+
+  /**
+   * @static
+   * Wrapper for localStorage.getItem(key)
+   * @param {string} key
+   * @param {Storage} storage localStorage or sessionStorage
+   * @returns {Object} Data by the key
+   * @description Read data from localStorage for a specific key
+   */
+  static get(key: string, storage = localStorage) {
+    return JSON.parse(<string>storage.getItem(key));
+  }
+
+  /**
+   * @static
+   * Wrapper for localStorage.setItem(key, value)
+   * @param {string} key
+   * @param {Object} value
+   * @param {Storage} storage localStorage or sessionStorage
+   * @description Adds data by key
+   */
+  static set(key: string, value: any, storage = localStorage) {
+    return storage.setItem(key, JSON.stringify(value));
+  }
+
+  /**
+   * @static
+   * Wrapper for localStorage.removeItem(key)
+   * @param {string} key
+   * @param {Storage} storage localStorage or sessionStorage
+   * @description Removes data by key
+   */
+  static remove(key: string, storage = localStorage) {
+    return storage.removeItem(key);
+  }
+}
