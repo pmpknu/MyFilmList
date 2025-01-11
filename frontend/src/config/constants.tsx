978fa802bc2740c9d89721fb53600806c5032284
pmpknu <dangorlyakov@gmail.com>
2025-01-11 16:49:05 +0300

    feat(config): add constants for application

---
+++ frontend/src/config/constants.tsx
@@ -0,0 +1,5 @@
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+export const TOKEN_KEY = 'MFL_TOKEN';
+
+export const REQUESTS_KEY = 'MFL_REQUESTS';
+export const REQUEST_UUIDS_HISTORY_LENGTH = 128;
