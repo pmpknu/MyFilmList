fc7cb60bfbf616c032173c5f50a4d53ad444d8c6
pmpknu <dangorlyakov@gmail.com>
2025-01-11 23:10:13 +0300

    feat(styles): add tailwind dependency

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,15 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
