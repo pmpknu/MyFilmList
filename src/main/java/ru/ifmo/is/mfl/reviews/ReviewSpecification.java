f72dc7ee9e18ac575e6281f7b14ff3d8d5704bbc
maxbarsukov <maximbarsukov@bk.ru>
2025-01-12 13:30:58 +0300

    feat(reviews): :sparkles: add `ReviewSpecification`
    #23

---
+++ src/main/java/ru/ifmo/is/mfl/reviews/ReviewSpecification.java
@@ -0,0 +1,29 @@
+package ru.ifmo.is.mfl.reviews;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import ru.ifmo.is.mfl.users.User;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewSpecification {
+
+  public Specification<Review> withVisible() {
+    return (root, query, cb) -> cb.equal(root.get("visible"), true);
+  }
+
+  public Specification<Review> withUser(int userId) {
+    return (root, query, cb) -> cb.equal(root.get("user").get("id"), userId);
+  }
+
+  public Specification<Review> withMovie(int movieId) {
+    return (root, query, cb) -> cb.equal(root.get("movie").get("id"), movieId);
+  }
+
+  public Specification<Review> visible(User currentUser) {
+    var currentUserId = currentUser == null ? -1 : currentUser.getId();
+    return withVisible().or(withUser(currentUserId));
+  }
+}
