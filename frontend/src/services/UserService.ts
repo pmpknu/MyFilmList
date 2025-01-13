ad1a4c9dfcfb5321cb6273be19aa599f3e85c1c5
pmpknu <dangorlyakov@gmail.com>
2025-01-14 00:05:59 +0300

    feat(user): add user service

--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -31,29 +31,29 @@ export default class UserService {
 
     /**
      * Get user by ID
-     * @param {string} id User ID
+     * @param {number} id User ID
      * @returns {Promise<AxiosResponse<UserDto>>} User data
      */
-    static async getUserById(id: string): Promise<AxiosResponse<UserDto>> {
+    static async getUserById(id: number): Promise<AxiosResponse<UserDto>> {
         return api.get<UserDto>(`/users/${id}`);
     }
 
     /**
      * Delete user by ID
-     * @param {string} id User ID
+     * @param {number} id User ID
      * @returns {Promise<AxiosResponse<void>>}
      */
-    static async deleteUser(id: string): Promise<AxiosResponse<void>> {
+    static async deleteUser(id: number): Promise<AxiosResponse<void>> {
         return api.delete<void>(`/users/${id}`);
     }
 
     /**
      * Update user by ID
-     * @param {string} id User ID
+     * @param {number} id User ID
      * @param {UpdateUserDto} userData Updated user data
      * @returns {Promise<AxiosResponse<UserDto>>} Updated user data
      */
-    static async updateUser(id: string, userData: UserUpdateDto): Promise<AxiosResponse<UserDto>> {
+    static async updateUser(id: number, userData: UserUpdateDto): Promise<AxiosResponse<UserDto>> {
         return api.patch<UserDto>(`/users/${id}`, userData);
     }
 
