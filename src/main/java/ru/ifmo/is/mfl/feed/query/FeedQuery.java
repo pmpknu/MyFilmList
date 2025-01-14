38d80983c5d0818050024cf420c72fffc48377e4
maxbarsukov <maximbarsukov@bk.ru>
2025-01-14 21:04:25 +0300

    feat(feed): :sparkles: implement watchlists recommendation in `FeedQuery`
    #30

--- src/main/java/ru/ifmo/is/mfl/feed/query/FeedQuery.java
+++ src/main/java/ru/ifmo/is/mfl/feed/query/FeedQuery.java
@@ -12,6 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.ifmo.is.mfl.movies.Movie;
 import ru.ifmo.is.mfl.movies.MovieRepository;
 import ru.ifmo.is.mfl.movieviews.MovieView;
+import ru.ifmo.is.mfl.watchlists.WatchList;
+import ru.ifmo.is.mfl.watchlists.WatchListRepository;
+import ru.ifmo.is.mfl.watchlists.WatchListSpecification;
 import ru.ifmo.is.mfl.users.User;
 
 import java.util.List;
@@ -21,6 +24,8 @@ import java.util.List;
 public class FeedQuery {
 
   private final MovieRepository movieRepository;
+  private final WatchListRepository watchListRepository;
+  private final WatchListSpecification watchListSpecification;
 
   @Transactional(readOnly = true)
   public Page<Movie> getRecommendedMovies(Pageable pageable, User currentUser) {
@@ -39,6 +44,24 @@ public class FeedQuery {
     );
   }
 
+  @Transactional(readOnly = true)
+  public Page<WatchList> getRecommendedWatchLists(Pageable pageable, User currentUser) {
+    var userId = currentUser != null ? currentUser.getId() : null;
+    var limit = pageable.getPageSize();
+    var offset = (int) pageable.getOffset();
+
+    List<WatchList> watchLists = watchListRepository.findRecommendedWatchLists(userId, limit, offset);
+
+    return PageableExecutionUtils.getPage(
+      watchLists,
+      pageable,
+      () -> watchListRepository.count(
+        notCreatedByUser(currentUser)
+          .and(watchListSpecification.withVisible())
+      )
+    );
+  }
+
   private Specification<Movie> notViewedByUser(User currentUser) {
     return (root, query, cb) -> {
       if (query == null) {
@@ -53,5 +76,9 @@ public class FeedQuery {
 
       return cb.not(root.get("id").in(subquery));
     };
+  }
+
+  private Specification<WatchList> notCreatedByUser(User currentUser) {
+    return (root, query, cb) -> cb.notEqual(root.get("user"), currentUser);
   }
 }
