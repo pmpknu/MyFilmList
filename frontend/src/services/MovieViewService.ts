22ba27c467399afb1c45e4c90ed57a56dfa882d9
pmpknu <dangorlyakov@gmail.com>
2025-01-14 15:22:36 +0300

    feat(services): add MovieView Service

---
+++ frontend/src/services/MovieViewService.ts
@@ -0,0 +1,51 @@
+import api from './api/api';
+import { AxiosResponse } from 'axios';
+import { MovieViewDto } from '@/interfaces/movieview/dto/MovieViewDto';
+import { MovieViewWithoutMovieDto } from '@/interfaces/movieview/dto/MovieViewWithoutMovieDto';
+import { MovieViewWithoutUserDto } from '@/interfaces/movieview/dto/MovieViewWithoutUserDto';
+import Paged from '@/interfaces/paged/models/Paged';
+import { createCrudUri } from '@/utils/uri';
+
+export default class MovieViewService {
+    /**
+     * Get all views for a movie
+     * @param {number} movieId Movie ID
+     * @param {number} page Page number
+     * @param {number} size Page size
+     * @param {string[]} sort Sorting parameters
+     * @returns {Promise<AxiosResponse<Paged<MovieViewWithoutMovieDto>>>} List of views for the movie
+     */
+    static async getViewsForMovie(movieId: number, page: number = 0, size: number = 20, sort: string[]): Promise<AxiosResponse<Paged<MovieViewWithoutMovieDto>>> {
+        return api.get<Paged<MovieViewWithoutMovieDto>>(`/api/movies/${movieId}/views${createCrudUri(page, size, sort)}`);
+    }
+
+    /**
+     * Mark a movie as viewed
+     * @param {number} movieId Movie ID
+     * @returns {Promise<AxiosResponse<MovieViewDto>>} Response confirming the view action
+     */
+    static async markMovieAsViewed(movieId: number): Promise<AxiosResponse<MovieViewDto>> {
+        return api.post<MovieViewDto>(`/api/movies/${movieId}/views`, {});
+    }
+
+    /**
+     * Unmark a movie as viewed
+     * @param {number} movieId Movie ID
+     * @returns {Promise<AxiosResponse<void>>} Response confirming the unview action
+     */
+    static async unmarkMovieAsViewed(movieId: number): Promise<AxiosResponse<void>> {
+        return api.delete<void>(`/api/movies/${movieId}/views`);
+    }
+
+    /**
+     * Get all views for a user
+     * @param {number} userId User ID
+     * @param {number} page Page number
+     * @param {number} size Page size
+     * @param {string[]} sort Sorting parameters
+     * @returns {Promise<AxiosResponse<Paged<MovieViewWithoutUserDto>>>} List of views for the user
+     */
+    static async getViewsForUser(userId: number, page: number = 0, size: number = 20, sort: string[]): Promise<AxiosResponse<Paged<MovieViewWithoutUserDto>>> {
+        return api.get<Paged<MovieViewWithoutUserDto>>(`/api/users/${userId}/views${createCrudUri(page, size, sort)}`);
+    }
+}
