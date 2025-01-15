fd9f9b995f5f67bde94bb209686d9c8e43f82e2a
pmpknu <dangorlyakov@gmail.com>
2025-01-15 07:19:13 +0300

    fix(service): fix FeedService paging

--- frontend/src/services/FeedService.ts
+++ frontend/src/services/FeedService.ts
@@ -3,21 +3,28 @@ import { AxiosResponse } from 'axios';
 import { MovieDto } from '@/interfaces/movie/dto/MovieDto';
 import { WatchListDto } from '@/interfaces/watchlist/dto/WatchListDto';
 import Paged from '@/interfaces/paged/models/Paged';
+import { createCrudUri } from '@/utils/uri';
 
 export default class FeedService {
     /**
      * Get recommended movies
+     * @param {number} page Page number
+     * @param {number} size Page size
+     * @param {string[]} sort Sorting parameters
      * @returns {Promise<AxiosResponse<Paged<MovieDto>>>} List of recommended movies
      */
-    static async getRecommendedMovies(): Promise<AxiosResponse<Paged<MovieDto>>> {
-        return api.get<Paged<MovieDto>>('/movies/feed');
+    static async getRecommendedMovies(page: number = 0, size: number = 20, sort: string[] = []): Promise<AxiosResponse<Paged<MovieDto>>> {
+        return api.get<Paged<MovieDto>>(`/movies/feed${createCrudUri(page, size, sort)}`);
     }
 
     /**
      * Get recommended watchlists
+     * @param {number} page Page number
+     * @param {number} size Page size
+     * @param {string[]} sort Sorting parameters
      * @returns {Promise<AxiosResponse<Paged<WatchListDto>>>} List of recommended watchlists
      */
-    static async getRecommendedWatchLists(): Promise<AxiosResponse<Paged<WatchListDto>>> {
-        return api.get<Paged<WatchListDto>>('/watchlists/feed');
+    static async getRecommendedWatchLists(page: number = 0, size: number = 20, sort: string[] = []): Promise<AxiosResponse<Paged<WatchListDto>>> {
+        return api.get<Paged<WatchListDto>>(`/watchlists/feed${createCrudUri(page, size, sort)}`);
     }
 }
