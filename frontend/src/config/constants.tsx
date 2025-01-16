5f7091b56d4b33e1bce8a627bd3f4af44f36ad34
pmpknu <dangorlyakov@gmail.com>
2025-01-16 16:04:40 +0300

    refactor(config): add api url

--- frontend/src/config/constants.tsx
+++ frontend/src/config/constants.tsx
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://mfl.maxbarsukov.ru/api';
 export const TOKEN_KEY = 'MFL_TOKEN';
 export const REFRESH_TOKEN_KEY = 'MFL_REFRESH';
 export const REQUESTS_KEY = 'MFL_REQUESTS';
