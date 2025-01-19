6677905cb1ed67a532609f8cc29164e064e0f71f
pmpknu <dangorlyakov@gmail.com>
2025-01-19 23:21:24 +0300

    fix(movies): remove `mt-12` spacer from top

--- client/src/features/movies/components/movie-view.tsx
+++ client/src/features/movies/components/movie-view.tsx
@@ -2,7 +2,7 @@ import MovieForm from '../movie-form';
 
 export default function MovieViewPage() {
   return (
-    <div className='mt-12 flex h-auto flex-col items-center justify-center bg-background p-6 md:p-10'>
+    <div className='flex h-auto flex-col items-center justify-center bg-background p-6 md:p-10'>
       <div className='w-full max-w-sm md:max-w-3xl'>
           <MovieForm />
       </div>
