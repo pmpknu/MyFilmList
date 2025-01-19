9005bb65f5606d94701c1636be8cb26a7ffcab06
pmpknu <dangorlyakov@gmail.com>
2025-01-20 02:50:34 +0300

    feat(watchlists): add grid

---
+++ client/src/features/watchlists/schemas/WatchListGrid.tsx
@@ -0,0 +1,45 @@
+// WatchlistGrid.tsx
+import { Spinner } from '@/components/ui/spinner';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import Poster from '@/features/movies/schemas/Poster';
+import { WatchListDto } from '@/interfaces/watchlist/dto/WatchListDto';
+
+interface WatchlistGridProps {
+  loading: boolean;
+  watchlists: WatchListDto[];
+  onClick: (id: number) => void;
+}
+
+export function WatchlistGrid({ loading, watchlists, onClick }: WatchlistGridProps) {
+  return (
+    <>
+      {loading ? (
+        <div className='flex justify-center'>
+          <Spinner size='large' />
+        </div>
+      ) : (
+        <ScrollArea className='h-[66vh] rounded-md border'>
+          <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {watchlists.map((watchlist) => (
+              <Poster
+                key={watchlist.id}
+                posterUrl={watchlist.photo}
+                title={watchlist.name}
+                onClick={() => onClick(watchlist.id)}
+                width={225}
+                height={500}
+                aspectRatio='square'
+              />
+            ))}
+
+            {watchlists.length === 0 && !loading && (
+              <p className='text-center text-gray-500'>
+                {watchlists.length === 0 ? 'Коллекции не найдены' : 'Попробуйте еще раз'}
+              </p>
+            )}
+          </div>
+        </ScrollArea>
+      )}
+    </>
+  );
+}
